93a47bf1395d85494b6f823009c660c2a46300dd
Alan Desktop <albertodro0909@gmail.com>
2023-01-26 09:23:18 -0600

    Logic created and Square component

---
+++ src/components/Square.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export function Square ({ children, isSelected, updateBoard, rowIndex, colIndex }) {
+  
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+  
+  const handleClick = () => {
+    updateBoard(rowIndex, colIndex);
+  }
+
+  return (
+    <div
+      className={className}
+      onClick={handleClick}
+    >
+      {children}
+    </div>
+  )
+}
